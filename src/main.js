e36de3e07acc78951069417337810a66a685b11d
NVRM0R <vovavelho@gmail.com>
2022-04-27 15:29:22 -0400

    workin?

--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
 import { createApp } from 'vue'
 
 import App from './App'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import components from "@/components/UI"
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import '@/app.scss'
 const app = createApp(App)
 
 components.forEach(component=>{
     app.component(component.name,component)
 })
-app.use(BootstrapVue);
-app.use(IconsPlugin);
 app.mount('#app')
